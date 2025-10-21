78e8afc56141acdef8151ffe9ad5896c35a01629
Ugo <maraway@gmail.com>
2025-10-22 00:39:26 +0100

    Add railway domain to ALLOWED_HOSTS

--- DevConnect/settings.py
+++ DevConnect/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security settings
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='devconnect-backend-production-d374.up.railway.app, localhost, 127.0.0.1').split(',')
 
 # Application definition
 INSTALLED_APPS = [
