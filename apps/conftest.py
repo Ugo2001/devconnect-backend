ceb52e72cc57dfe9863fd2dc5840dabab4efc809
Ugo <maraway@gmail.com>
2025-10-22 14:24:28 +0100

    Propre correction

--- apps/conftest.py
+++ apps/conftest.py
@@ -1,4 +1,6 @@
 # apps/conftest.py
+import os
+import redis
 import pytest
 from rest_framework.test import APIClient
 
@@ -17,3 +19,24 @@ def enable_db_access_for_all_tests(db):
     Give all tests access to the database
     """
     pass
+
+@pytest.fixture(scope='session')
+def redis_client():
+    """
+    Fixture that provides a Redis client for testing
+    """
+    
+    host = os.environ.get('REDIS_HOST', 'redis')
+    client = redis.Redis(host=host, port=6379, decode_responses=True, db=1)  # Use a separate DB for tests
+
+    # test connectivity
+    try:
+        client.ping()
+    except redis.ConnectionError as e:
+        pytest.fail(f"Could not connect to Redis at {host}:6379 - {e}")
+
+    yield client
+
+    # Teardown: flush the test database
+    client.flushdb()
+    client.close()
